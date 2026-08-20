13031a928cef42c980d491d3708eb32f4dad988e
Dave Plater <davejplater@gmail.com>
2026-08-20 15:30:16 +0200

    Update main.c

--- main/main.c
+++ main/main.c
@@ -19,9 +19,6 @@ void app_main(void)
 	hd44780_clear(&lcd_dev);
 	lcd_write_string("0123456789ABCDEF");
 	ESP_ERROR_CHECK(nvs_flash_init());
- R1ON;
- dly_msec(1000);
- R1OFF;
 	if(retrieve_error() == 1)
 	{
 		call_joe(HOPPER_EMT, 6);
